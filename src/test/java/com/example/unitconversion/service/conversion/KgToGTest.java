70e37d1f5d8ba3b0562d9e880622ba7b559cff12
Viktor Weindlmayr <viktor.weindlmayr@digitalsunray.com>
2022-03-10 21:18:14 +0100

    implemented feature with tests

---
+++ src/test/java/com/example/unitconversion/service/conversion/KgToGTest.java
@@ -0,0 +1,45 @@
+package com.example.unitconversion.service.conversion;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KgToGTest {
+
+    KgToG kgToG;
+
+    @BeforeEach
+    void setup() {
+        kgToG = new KgToG();
+    }
+
+    @Test
+    void getConversionCodeTest() {
+        assertEquals("kg to g", kgToG.getConversionCode());
+    }
+
+    @Test //the test passes even if the from and to Type are completely wrong?
+    void executeKgToG() throws Exception {
+        assertEquals(2000f, kgToG.execute(2f, "kg", "g"));
+    }
+
+    @Test
+    void executeKgToGWithComma() throws Exception {
+        assertEquals(2123.456f, kgToG.execute(2.123456f, "kg", "g"));
+    }
+
+    @Test
+    void executeKgToGWithNegativeValue() throws Exception { //why don't I need the throws Exception here?
+        assertThrows(ResponseStatusException.class,
+        () -> kgToG.execute(-2f, "kg", "g"), "value < 0 throw Exception");
+    }
+
+    @Test
+    void executeKgToGWithZeroValue() { //why don't I need the throws Exception here?
+        assertThrows(ResponseStatusException.class,
+                () -> kgToG.execute(0f, "kg", "g"), "value == 0 throw Exception");
+    }
+
+}
