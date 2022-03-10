cb451bd88046995d011a8f563d92cdd2327b9397
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2022-03-10 19:38:38 +0100

    use float compare method

--- src/main/java/com/example/unitconversion/service/conversion/CToF.java
+++ src/main/java/com/example/unitconversion/service/conversion/CToF.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import static java.lang.Float.compare;
+
 @Service
 public class CToF implements ConversionStrategy {
 
@@ -17,8 +19,8 @@ public class CToF implements ConversionStrategy {
 
     @Override
     public float execute(Float fromValue, String fromType, String toType) throws Exception {
-        if (fromValue < -273.15) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "From Value must be greater than -273.15°C");
+        if (compare(fromValue, -273.15f) < 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "From Value must be greater than or equal to -273.15°C");
         }
         return fromValue * 9/5 + 32;
     }
