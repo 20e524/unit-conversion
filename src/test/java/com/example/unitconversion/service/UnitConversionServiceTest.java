77e40062492fe91ca7e12161ade944dc9e43da06
Viktor Weindlmayr <viktor.weindlmayr@digitalsunray.com>
2022-03-31 10:45:13 +0200

    additional tests

--- src/test/java/com/example/unitconversion/service/UnitConversionServiceTest.java
+++ src/test/java/com/example/unitconversion/service/UnitConversionServiceTest.java
@@ -3,8 +3,7 @@ package com.example.unitconversion.service;
 import com.example.unitconversion.dto.Request;
 import com.example.unitconversion.strategy.ConversionStrategy;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.*;
 import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
@@ -17,6 +16,7 @@ class UnitConversionServiceTest {
     UnitConversionService unitConversionService;
     ConversionStrategy conversionStrategy;
 
+    /*
     @BeforeEach
     void setup() {
         conversionStrategy = mock(ConversionStrategy.class);
@@ -25,17 +25,70 @@ class UnitConversionServiceTest {
         list.add(conversionStrategy);
         unitConversionService = new UnitConversionService(list);
     }
+     */
 
     @Test
-    void convertCallsExecuteMethod() throws Exception {
+    void convertCallsExecuteMethodGToKG() throws Exception {
+        conversionStrategy = mock(ConversionStrategy.class);
+        when(conversionStrategy.getConversionCode()).thenReturn("g to kg");
+        List<ConversionStrategy> list = new ArrayList<>();
+        list.add(conversionStrategy);
+        unitConversionService = new UnitConversionService(list);
+
         Request mockRequest = new Request(55000f, "g", "kg");
+        unitConversionService.convert(mockRequest);
+        verify(conversionStrategy, times(1)).execute(anyFloat(), any(), any());
+    }
+
+
+    @Test
+    void convertCallsExecuteMethodKGToG() throws Exception {
+        conversionStrategy = mock(ConversionStrategy.class);
+        when(conversionStrategy.getConversionCode()).thenReturn("kg to g");
+        List<ConversionStrategy> list = new ArrayList<>();
+        list.add(conversionStrategy);
+        unitConversionService = new UnitConversionService(list);
+
+        Request mockRequest = new Request(55000f, "kg", "g");
+        unitConversionService.convert(mockRequest);
+        verify(conversionStrategy, times(1)).execute(anyFloat(), any(), any());
+    }
+
+    @Test
+    void convertCallsExecuteMethodFToC() throws Exception {
+        conversionStrategy = mock(ConversionStrategy.class);
+        when(conversionStrategy.getConversionCode()).thenReturn("F to C");
+        List<ConversionStrategy> list = new ArrayList<>();
+        list.add(conversionStrategy);
+        unitConversionService = new UnitConversionService(list);
+
+        Request mockRequest = new Request(55000f, "F", "C");
+        unitConversionService.convert(mockRequest);
+        verify(conversionStrategy, times(1)).execute(anyFloat(), any(), any());
+    }
+
+    @Test
+    void convertCallsExecuteMethodCToF() throws Exception {
+        conversionStrategy = mock(ConversionStrategy.class);
+        when(conversionStrategy.getConversionCode()).thenReturn("C to F");
+        List<ConversionStrategy> list = new ArrayList<>();
+        list.add(conversionStrategy);
+        unitConversionService = new UnitConversionService(list);
+
+        Request mockRequest = new Request(55000f, "C", "F");
         unitConversionService.convert(mockRequest);
         verify(conversionStrategy, times(1)).execute(anyFloat(), any(), any());
     }
 
     @Test
     void convertThrowsExceptionWrongInput() throws Exception {
-        Request mockRequest = new Request(55000f, "hh", "kg");
+        conversionStrategy = mock(ConversionStrategy.class);
+        when(conversionStrategy.getConversionCode()).thenReturn("g to kg");
+        List<ConversionStrategy> list = new ArrayList<>();
+        list.add(conversionStrategy);
+        unitConversionService = new UnitConversionService(list);
+
+        Request mockRequest = new Request(55000f, "gg", "kg");
         assertThrows(Exception.class, () -> unitConversionService.convert(mockRequest));
     }
 }
